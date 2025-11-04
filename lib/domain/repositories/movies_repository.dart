542e0f5f4cc482524bc898ab8a2660f59a3aed9f
Neftali Vergara <neftaliarturohernandez@gmail.com>
2025-11-03 18:35:00 -0600

    feat: Reorganize method declarations in MoviesRepository for improved readability

--- lib/domain/repositories/movies_repository.dart
+++ lib/domain/repositories/movies_repository.dart
@@ -4,9 +4,11 @@ import 'package:cinemapedia_220100/domain/entities/movie.dart';
 // Se encarga de declarar los métodos que obtienen datos del datasource,
 // en este caso, las películas que están en cartelera (Now Playing).
 abstract class MoviesRepository {
-
-// Método que deberá implementarse para obtener la lista de películas actuales.
-// El parámetro [page] permite manejar la paginación de resultados.
-Future<List<Movie>> getNowPlaying({ int page = 1 });
-
+  // Método que deberá implementarse para obtener la lista de películas actuales.
+  // El parámetro [page] permite manejar la paginación de resultados.
+  Future<List<Movie>> getNowPlaying({int page = 1});
+  Future<List<Movie>> getPopular({int page = 1});
+  Future<List<Movie>> getUpcoming({int page = 1});
+  Future<List<Movie>> getTopRated({int page = 1});
+  Future<List<Movie>> getMexicanMovies({int page = 1});
 }
